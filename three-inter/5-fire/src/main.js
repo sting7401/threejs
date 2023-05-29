371c4704864e79706c5a45f7c9e3023523356e23
sting7401 <sting7401@naver.com>
2023-05-29 16:53:32 +0900

    d

--- three-inter/5-fire/src/main.js
+++ three-inter/5-fire/src/main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls';
+
+import Firework from './Firework.js';
 
 const init = async () => {
 	const canvas = document.querySelector('#canvas');
@@ -8,9 +10,6 @@ const init = async () => {
 		antialias: true,
 		canvas,
 	});
-
-	// const gui = new GUI();
-	const clock = new THREE.Clock();
 
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.shadowMap.enabled = true;
@@ -22,26 +21,30 @@ const init = async () => {
 		75,
 		window.innerWidth / window.innerHeight,
 		1,
-		500,
+		10000,
 	);
-	camera.position.set(0, 0, 5);
+	camera.position.z = 8000;
 
-	// controls
+	const fireworks = [];
 
-	new OrbitControls(camera, renderer.domElement);
+	fireworks.update = function () {
+		for (let index = 0; index < this.length; index += 1) {
+			const firework = fireworks[index];
 
-	const geometry = new THREE.SphereGeometry();
-	const material = new THREE.PointsMaterial({
-		color: 0xccaaff,
-		size: 0.01,
-		sizeAttenuation: true,
-	});
-	const points = new THREE.Points(geometry, material);
+			firework.update();
+		}
+	};
 
-	scene.add(points);
+	let firework = new Firework({ x: 0, y: 0 });
+
+	scene.add(firework.points);
+
+	fireworks.push(firework);
 
 	// renderer
 	const render = () => {
+		fireworks.update();
+
 		renderer.render(scene, camera);
 		requestAnimationFrame(render);
 	};
@@ -49,6 +52,7 @@ const init = async () => {
 	render();
 
 	const handleResize = () => {
+		firework.update();
 		camera.aspect = window.innerWidth / window.innerHeight;
 		camera.updateProjectionMatrix();
 
@@ -56,7 +60,19 @@ const init = async () => {
 		renderer.render(scene, camera);
 	};
 
+	const handleMouseDown = () => {
+		firework = new Firework({
+			x: THREE.MathUtils.randFloatSpread(8000),
+			y: THREE.MathUtils.randFloatSpread(8000),
+		});
+
+		scene.add(firework.points);
+
+		fireworks.push(firework);
+	};
+
 	window.addEventListener('resize', handleResize);
+	window.addEventListener('mousedown', handleMouseDown);
 };
 
 window.addEventListener('load', () => {
